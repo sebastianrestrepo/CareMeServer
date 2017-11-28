9bac7abe5931f6814ef085df2d413dbf677605c4
Sebastián Restrepo <sebastianrestrepoq@gmail.com>
2017-11-28 16:34:29 -0500

    Arduino library working

--- CareMeServer/src/main/java/ArduinoConection.java
+++ CareMeServer/src/main/java/ArduinoConection.java
@@ -1,8 +1,10 @@
 import processing.core.PApplet;
+import processing.serial.Serial;
 
 public class ArduinoConection {
 
 	private PApplet app;
+	private Serial puerto;
 	
 	public ArduinoConection(PApplet app) {
 		this.app = app;
